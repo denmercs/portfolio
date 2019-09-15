57119695444999b9d1a014460cec0d5675023815
denmercs <denmercs@gmail.com>
2019-09-15 12:21:28 -0500

    done with layout and design at home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,7 @@ import "../pages/home.scss";
 import {Link} from "react-router-dom";
 import {Container} from "@material-ui/core";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-// import {faGithub} from "@fortawesome/free-solid-svg-icons";
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 /**
  * PLAN is to import data from the backend 
  */
@@ -11,20 +11,19 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 const Home = () => {
     return (
         <Container className="home">
-            <img src={require("../assets/dennis.png")} alt="Dennis" className="home-img"/>
-            <div className="home-btn">
-                <Link>Coding</Link>
-                <Link>Design</Link>
+            <div className="home-hero">
+                <Link to="/projects" className="home-hero-btn">	&lt;Coding&gt;</Link>
+                <img src={require("../assets/dennis.png")} alt="Dennis" className="home-hero-img"/>
+                <Link to="/designs" className="home-hero-btn">Design</Link>
             </div>
-            <h1>Hi! I'm Dennis</h1>
             
             <p>“Formal education will make you a living; 
                 self-education will make you a fortune.”</p> 
 
             <div className="home-notes">
-                <p>Technologies used on this site are: React, HTML, CSS, SASS, GIT, NPM. Github repo here: </p>
+                <p>Technologies used on this site are: HTML, CSS, SASS, React, Node, GIT, NPM. Github repo here: </p>
                 <a href="https://github.com/denmercs/portfolio" target="_blank">
-                    <FontAwesomeIcon className="f-awesome" icon="coffee" />
+                    <FontAwesomeIcon icon={faCoffee} className="f-coffee"/>
                 </a>
             </div>
         </Container> 
