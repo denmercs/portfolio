815772302f8d8cfee6ba12dc7dd151b85f537a77
denmercs <denmercs@gmail.com>
2020-03-31 20:28:45 -0500

    changes implemented base from career staff suggestions

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -24,12 +24,6 @@ const Project = prop => {
           <div className="card-info">
             <h4>{company_name}</h4>
             <p className="application">{application}</p>
-            <p>{description}</p>
-            <p>Location: {location}</p>
-            <p>Role: {role}</p>
-            <p>Stack: {stack}</p>
-            {api !== "" ? <p>API/Library: {api}</p> : null}
-
             <div className="card-links">
               <a href={repo} target="_blank" rel="noopener noreferrer">
                 <i className="fa fa-github" aria-hidden="true"></i>
@@ -40,6 +34,11 @@ const Project = prop => {
                 <p>Website</p>
               </a>
             </div>
+            <p>{description}</p>
+            <p>Location: {location}</p>
+            <p>Role: {role}</p>
+            <p>Stack: {stack}</p>
+            {api !== "" ? <p>API/Library: {api}</p> : null}
           </div>
           <div className="video-container photo-container">
             {/\.(gif|jpg|jpeg|tiff|png)$/i.test(source) ? (
